478371b823b54e76b686e30de84356f2a268abe7
tibast <qaultsabitm@gmail.com>
2024-12-29 02:10:56 +0700

    fix typo

--- migrations/20241228221311_create_subscriptions_table.sql
+++ migrations/20241228221311_create_subscriptions_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE subscriptions(
     id uuid PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
-    subsribed_at TIMESTAMPTZ NOT NULL
+    subscribed_at TIMESTAMPTZ NOT NULL
 );
