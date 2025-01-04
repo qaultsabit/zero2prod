266dfbac2dca762216e7fa4809338da80fa68ebc
tibast <qaultsabitm@gmail.com>
2025-01-05 06:32:26 +0700

    add validation for name

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -133,3 +133,32 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "name is empty"),
+        ("name=le%20guin&email=", "email is empty"),
+        ("name=&email=", "name and email are empty"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when {}.",
+            error_message
+        );
+    }
+}
