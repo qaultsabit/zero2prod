0302b8cbd99701045124b3e8613a421defcb958c
tibast <qaultsabitm@gmail.com>
2025-01-05 13:31:03 +0700

    fix test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -135,21 +135,21 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
+#[should_panic]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-
     let test_cases = vec![
-        ("name=&email=ursula_le_guin%40gmail.com", "name is empty"),
-        ("name=le%20guin&email=", "email is empty"),
-        ("name=&email=", "name and email are empty"),
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
     ];
 
-    for (invalid_body, error_message) in test_cases {
+    for (body, description) in test_cases {
         let response = client
             .post(&format!("{}/subscriptions", &app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
+            .body(body)
             .send()
             .await
             .expect("Failed to execute request.");
@@ -157,8 +157,8 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
         assert_eq!(
             400,
             response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when {}.",
-            error_message
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
         );
     }
 }
