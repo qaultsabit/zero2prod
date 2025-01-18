ae9bbb4651dd199105e87986651fd0128f8c7e60
tibast <qaultsabitm@gmail.com>
2025-01-19 02:30:14 +0700

    delete a cookie

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,5 +1,9 @@
 use crate::startup::HmacSecret;
-use actix_web::{http::header::ContentType, web, HttpResponse};
+use actix_web::{
+    cookie::{time::Duration, Cookie},
+    http::header::ContentType,
+    HttpRequest, HttpResponse,
+};
 use hmac::{Hmac, Mac};
 use secrecy::ExposeSecret;
 
@@ -23,28 +27,16 @@ impl QueryParams {
     }
 }
 
-pub async fn login_form(
-    query: Option<web::Query<QueryParams>>,
-    secret: web::Data<HmacSecret>,
-) -> HttpResponse {
-    let error_html = match query {
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
         None => "".into(),
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error) => {
-                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
-            }
-            Err(e) => {
-                tracing::warn!(
-                error.message = %e,
-                error.cause_chain = ?e,
-                "Failed to verify query parameters using the HMAC tag"
-                );
-                "".into()
-            }
-        },
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
+        }
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
+        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
             r#"<!DOCTYPE html>
 <html lang="en">
@@ -73,5 +65,9 @@ pub async fn login_form(
     </form>
 </body>
 </html>"#,
-        ))
+        ));
+    response
+        .add_removal_cookie(&Cookie::new("_flash", ""))
+        .unwrap();
+    response
 }
