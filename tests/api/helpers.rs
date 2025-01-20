9483ddaba7d390762dee9ce92e9653cf8c200ecd
tibast <qaultsabitm@gmail.com>
2025-01-20 22:05:38 +0700

    reject unautheticated users

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -148,6 +148,14 @@ impl TestApp {
             .await
             .unwrap()
     }
+
+    pub async fn get_admin_dashboard(&self) -> reqwest::Response {
+        self.api_client
+            .get(&format!("{}/admin/dashboard", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
