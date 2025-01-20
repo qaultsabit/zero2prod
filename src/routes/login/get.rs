8c5e60dc73c3e01443b331fae093501aea148b1b
tibast <qaultsabitm@gmail.com>
2025-01-20 20:50:18 +0700

    actix web flash messages

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -4,8 +4,10 @@ use actix_web::{
     http::header::ContentType,
     HttpRequest, HttpResponse,
 };
+use actix_web_flash_messages::{IncomingFlashMessages, Level};
 use hmac::{Hmac, Mac};
 use secrecy::ExposeSecret;
+use std::fmt::Write;
 
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
@@ -27,14 +29,12 @@ impl QueryParams {
     }
 }
 
-pub async fn login_form(request: HttpRequest) -> HttpResponse {
-    let error_html = match request.cookie("_flash") {
-        None => "".into(),
-        Some(cookie) => {
-            format!("<p><i>{}</i></p>", cookie.value())
-        }
-    };
-    let mut response = HttpResponse::Ok()
+pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
+    let mut error_html = String::new();
+    for m in flash_messages.iter().filter(|m| m.level() == Level::Error) {
+        writeln!(error_html, "<p><i>{}</i></p>", m.content()).unwrap();
+    }
+    HttpResponse::Ok()
         .content_type(ContentType::html())
         .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
@@ -65,9 +65,5 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
     </form>
 </body>
 </html>"#,
-        ));
-    response
-        .add_removal_cookie(&Cookie::new("_flash", ""))
-        .unwrap();
-    response
+        ))
 }
