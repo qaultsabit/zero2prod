25a9918fad5e79444b15d805f7dddfd46ca047b8
tibast <qaultsabitm@gmail.com>
2025-01-20 21:47:35 +0700

    session

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -137,6 +137,17 @@ impl TestApp {
             .await
             .unwrap()
     }
+
+    pub async fn get_admin_dashboard_html(&self) -> String {
+        self.api_client
+            .get(&format!("{}/admin/dashboard", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+            .text()
+            .await
+            .unwrap()
+    }
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
