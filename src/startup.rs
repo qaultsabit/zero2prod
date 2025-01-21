7b6169b05804351112395fba999e916712bb3249
tibast <qaultsabitm@gmail.com>
2025-01-21 20:12:19 +0700

    password reset

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::admin_dashboard;
+use crate::routes::{admin_dashboard, change_password, change_password_form};
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
@@ -103,6 +103,8 @@ async fn run(
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/admin/dashboard", web::get().to(admin_dashboard))
+            .route("/admin/password", web::get().to(change_password_form))
+            .route("/admin/password", web::post().to(change_password))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
