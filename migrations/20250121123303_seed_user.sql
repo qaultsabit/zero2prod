9ea2c1f157322061f1788bbea444f4a5923f24e6
tibast <qaultsabitm@gmail.com>
2025-01-21 19:48:55 +0700

    seed user

---
+++ migrations/20250121123303_seed_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    '0a5adabf-8a95-427e-9a27-ca5102b1dd80',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$Q2VzYVJlZ2lzdHJ5$3Q6Z'
+    '88fc2157-bc65-4d4c-b5f3-5d56011fc5a0'
+)
